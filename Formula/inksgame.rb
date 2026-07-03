1885245867ac4745e50a5b4369488dc0ca80c8ec
Luca295 <161210679+ls295dev@users.noreply.github.com>
2026-07-03 20:42:20 +0200

    Add Inksgame formula for game design in Inkscape

---
+++ Formula/inksgame.rb
@@ -0,0 +1,12 @@
+class Inksgame < Formula
+    desc "design games in Inkscape"
+    homepage "https://codeberg.org/Luca295/inksgame"
+    head "https://codeberg.org/Luca295/inksgame/archive/master.zip"
+    
+    depends_on "go" => :build
+    depends_on "inksgame-helper"
+
+    def install
+        system "go", "build", *std_go_args(ldflags: "-X main.version=HEAD")
+    end
+end
