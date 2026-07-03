a754ad190e0641d8531a0661efc006c631e80a99
Luca295 <161210679+ls295dev@users.noreply.github.com>
2026-07-03 20:35:11 +0200

    Add InksgameHelper formula for Homebrew

---
+++ Formula/inksgame-helper.rb
@@ -0,0 +1,17 @@
+class InksgameHelper < Formula
+    desc "dependency for inksgame"
+    homepage "https://codeberg.org/Luca295/inksgame-helper"
+    head "https://codeberg.org/Luca295/inksgame-helper/archive/master.zip"
+  
+    depends_on "node" => [:build, :test]
+
+    def install
+      
+        if build.head?
+          inreplace "package.json", /"version":\s*".*"/, '"version": "HEAD"'
+        end
+      
+        system "npm", "install", *std_npm_args
+        bin.install_symlink libexec/"bin/inksgame-helper"
+    end
+end
