515e9f122994412b6c9e2a9973048177b8ee5cd2
Luca295 <lucaschnell@aol.com>
2026-07-07 00:32:34 +0200

    change inksgame-helper to fixed release

--- Formula/inksgame-helper.rb
+++ Formula/inksgame-helper.rb
@@ -1,16 +1,11 @@
 class InksgameHelper < Formula
     desc "dependency for inksgame"
     homepage "https://codeberg.org/Luca295/inksgame-helper"
-    head "https://codeberg.org/Luca295/inksgame-helper/archive/main.zip"
+    url "https://codeberg.org/Luca295/inksgame-helper/archive/alpha2.zip"
   
     depends_on "node" => [:build, :test]
 
     def install
-      
-        if build.head?
-          inreplace "package.json", /"version":\s*".*"/, '"version": "HEAD"'
-        end
-      
         system "npm", "install", *std_npm_args
         bin.install_symlink libexec/"bin/inksgame-helper"
     end
